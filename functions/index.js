4e02c94e42f69d61a829ac9e915bb206983680a1
RexLou <cuales.166797@caloocan.sti.edu.ph>
2022-11-01 12:24:09 +0800

    testing log in with axios

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 const express = require("express");
 const app = express("express");
 const cors = require("cors");
---
+++ functions/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
