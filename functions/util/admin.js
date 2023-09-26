a4929c258f290caf3f9eba929a538e3ee2a85af4
JM ENTERESO <jentereso.kezar@gmail.com>
2023-09-26 22:25:25 +0800

    update

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./accueba-db-firebase-adminsdk-iv5o4-5d5717c76c.json");
+const serviceAccount = require("./accueba-new-firebase-adminsdk-hs3hz-31515814b2.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
